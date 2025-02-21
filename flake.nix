df2e669e48f868e91a4abc08bf8bb8c641e68668
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:19 +0000

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     extra-trusted-substituters = [
       "https://cache.dataaturservice.se/spectrum/"
       "https://cache.nixos.org/"
-      "https://deckcheatz-nightlies.cachix.org"
       "https://deploy-rs.cachix.org/"
       "https://devenv.cachix.org"
       "https://nix-community.cachix.org"
@@ -21,7 +20,6 @@
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "deckcheatz-nightlies.cachix.org-1:ygkraChLCkqqirdkGjQ68Y3LgVrdFB2bErQfj5TbmxU="
       "deploy-rs.cachix.org-1:xfNobmiwF/vzvK1gpfediPwpdIP0rpDV2rYqx40zdSI="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
