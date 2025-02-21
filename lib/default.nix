bc1e1c9cfca5fd6e05b749b72c9275963831bbaf
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:19 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,14 @@
 # SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
 #
 # SPDX-License-Identifier: GPL-3.0-only
-
 {
   self,
   inputs,
   pkgs ? null,
   ...
-}:
-rec {
-  inherit (pkgs.stdenv.hostPlatform)
+}: rec {
+  inherit
+    (pkgs.stdenv.hostPlatform)
     isLinux
     isDarwin
     isx86_64
@@ -50,9 +49,11 @@ rec {
   isPCx64 = isx86_64;
   isPCx32 = isi686;
   forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-  homePrefix = if isDarwin then "/Users" else "/home";
-  genPkgs =
-    system:
+  homePrefix =
+    if isDarwin
+    then "/Users"
+    else "/home";
+  genPkgs = system:
     import inputs.nixpkgs {
       inherit system;
       overlays = builtins.attrValues self.overlays;
@@ -60,39 +61,38 @@ rec {
     };
   hasRole = r: allRoles: builtins.elem r allRoles;
   hasRoles = rs: allRoles: builtins.all (role: hasRole role allRoles) rs;
-  mkHost =
-    {
-      type ? "nixos",
-      address ? null,
-      hostname ? null,
-      hostPlatform ? "x86_64-linux",
-      username ? "dzrodriguez",
-      baseModules ? [
-        inputs.agenix.nixosModules.default
-        inputs.auto-cpufreq.nixosModules.default
-        {
-          environment.systemPackages = [
-            inputs.agenix.packages.${hostPlatform}.default
-            inputs.nix-alien.packages.${hostPlatform}.nix-alien
-          ];
-        }
-        ../common
-        inputs.nixfigs-secrets.system
-      ],
-      monolithConfig ? true,
-      overlays ? [ ],
-      hostRoles ? [ "workstation" ],
-      hardwareModules ? [ ],
-      extraModules ? [ ],
-      pubkey ? null,
-      remoteBuild ? true,
-      deployable ? false,
-      embedHm ? false,
-    }:
-    if type == "nixos" then
-      assert address != null;
-      assert (hasSuffix "linux" hostPlatform);
+  mkHost = {
+    type ? "nixos",
+    address ? null,
+    hostname ? null,
+    hostPlatform ? "x86_64-linux",
+    username ? "dzrodriguez",
+    baseModules ? [
+      inputs.agenix.nixosModules.default
+      inputs.auto-cpufreq.nixosModules.default
       {
+        environment.systemPackages = [
+          inputs.agenix.packages.${hostPlatform}.default
+          inputs.nix-alien.packages.${hostPlatform}.nix-alien
+        ];
+      }
+      ../common
+      inputs.nixfigs-secrets.system
+    ],
+    monolithConfig ? true,
+    overlays ? [],
+    hostRoles ? ["workstation"],
+    hardwareModules ? [],
+    extraModules ? [],
+    pubkey ? null,
+    remoteBuild ? true,
+    deployable ? false,
+    embedHm ? false,
+  }:
+    if type == "nixos"
+    then
+      assert address != null;
+      assert (hasSuffix "linux" hostPlatform); {
         inherit
           address
           baseModules
@@ -111,10 +111,10 @@ rec {
           username
           ;
       }
-    else if type == "darwin" then
+    else if type == "darwin"
+    then
       assert pubkey != null && address != null;
-      assert (hasSuffix "darwin" hostPlatform);
-      {
+      assert (hasSuffix "darwin" hostPlatform); {
         inherit
           address
           baseModules
@@ -130,10 +130,10 @@ rec {
           username
           ;
       }
-    else if type == "home-manager" then
+    else if type == "home-manager"
+    then
       assert ((hasSuffix "linux" hostPlatform) || (hasSuffix "darwin" hostPlatform) && hostname == null);
-      assert pubkey == null;
-      {
+      assert pubkey == null; {
         inherit
           deployable
           hostPlatform
@@ -143,6 +143,5 @@ rec {
           username
           ;
       }
-    else
-      throw "unknown host type '${type}'";
+    else throw "unknown host type '${type}'";
 }
