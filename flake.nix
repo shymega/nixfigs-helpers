ff16b1b97c3a915f43ab01eccb315e16d6dd008b
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:19 +0000

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ flake.nix
@@ -0,0 +1,130 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{
+  description = "Helper functions for my NixOS flakes";
+
+  nixConfig = {
+    extra-trusted-substituters = [
+      "https://cache.dataaturservice.se/spectrum/"
+      "https://cache.nixos.org/"
+      "https://deckcheatz-nightlies.cachix.org"
+      "https://deploy-rs.cachix.org/"
+      "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://nix-on-droid.cachix.org"
+      "https://numtide.cachix.org"
+      "https://pre-commit-hooks.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "deckcheatz-nightlies.cachix.org-1:ygkraChLCkqqirdkGjQ68Y3LgVrdFB2bErQfj5TbmxU="
+      "deploy-rs.cachix.org-1:xfNobmiwF/vzvK1gpfediPwpdIP0rpDV2rYqx40zdSI="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "spectrum-os.org-2:foQk3r7t2VpRx92CaXb5ROyy/NBdRJQG2uX2XJMYZfU="
+    ];
+  };
+
+  outputs =
+    inputs:
+    let
+      inherit (inputs) self;
+      genPkgs =
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          config = self.nixpkgs-config;
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+        "riscv64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      treeFmtEachSystem =
+        f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
+      treeFmtEval = treeFmtEachSystem (pkgs: inputs.treefmt-nix.lib.evalModule pkgs ./nix/formatter.nix);
+
+      forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
+      forAllSystems = inputs.nixpkgs.lib.genAttrs allSystems;
+    in
+    {
+      helpers = {
+        formatter = ./nix/formatter.nix;
+        checks = ./nix/checks.nix;
+        _24-05-compat = import ./nix/24.05-compat.nix { inherit (inputs.nixpkgs) lib; };
+        devShells = ./nix/devshell.nix;
+      };
+      libx = forAllSystems (
+        system:
+        let
+          pkgs = genPkgs system;
+        in
+        import ./lib { inherit self inputs pkgs; }
+      );
+      # for `nix fmt`
+      formatter = treeFmtEachSystem (pkgs: treeFmtEval.${pkgs.system}.config.build.wrapper);
+      # for `nix flake check`
+      checks =
+        treeFmtEachSystem (pkgs: {
+          formatting = treeFmtEval.${pkgs.system}.config.build.wrapper;
+        })
+        // forEachSystem (system: {
+          pre-commit-check = import ./nix/checks.nix {
+            inherit self system inputs;
+            inherit (inputs.nixpkgs) lib;
+          };
+        });
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = genPkgs system;
+        in
+        import ./nix/devshell.nix { inherit pkgs self system; }
+      );
+      nixpkgs-config = {
+        allowUnfree = true;
+        allowUnsupportedSystem = true;
+        allowBroken = true;
+        allowInsecurePredicate = _: true;
+      };
+    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+}
