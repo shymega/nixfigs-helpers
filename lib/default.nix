c54eebd721f47aabaddef86898946b4b8d2d073f
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:20 +0000

    chore: Tidy up helper
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- lib/default.nix
+++ lib/default.nix
@@ -50,7 +50,7 @@
   isPC64 = isx86_64;
   isPC32 = isi686;
   isDarwinArm = pkgs.system == "aarch64-darwin";
-  isDarwinx86 = pkgs.system == "x86_64-darwin";``
+  isDarwinx86 = pkgs.system == "x86_64-darwin";
   forEachSystem = genAttrs systems;
   forAllEachSystems = genAttrs allSystems;
   homePrefix =
@@ -63,89 +63,6 @@
       overlays = builtins.attrValues self.overlays;
       config = self.nixpkgs-config;
     };
-  mkHost = {
-    type ? "nixos",
-    address ? null,
-    hostname ? null,
-    hostPlatform ? "x86_64-linux",
-    username ? "dzrodriguez",
-    baseModules ? [
-      inputs.agenix.nixosModules.default
-      inputs.auto-cpufreq.nixosModules.default
-      {
-        environment.systemPackages = [
-          inputs.agenix.packages.${hostPlatform}.default
-          inputs.nix-alien.packages.${hostPlatform}.nix-alien
-        ];
-      }
-      ../common
-      inputs.nixfigs-secrets.system
-    ],
-    monolithConfig ? true,
-    overlays ? [],
-    hostRoles ? ["workstation"],
-    hardwareModules ? [],
-    extraModules ? [],
-    pubkey ? null,
-    remoteBuild ? true,
-    deployable ? false,
-    embedHm ? false,
-  }:
-    if type == "nixos"
-    then
-      assert address != null;
-      assert (hasSuffix "linux" hostPlatform); {
-        inherit
-          address
-          baseModules
-          deployable
-          embedHm
-          extraModules
-          hardwareModules
-          hostPlatform
-          hostRoles
-          hostname
-          monolithConfig
-          overlays
-          pubkey
-          remoteBuild
-          type
-          username
-          ;
-      }
-    else if type == "darwin"
-    then
-      assert pubkey != null && address != null;
-      assert (hasSuffix "darwin" hostPlatform); {
-        inherit
-          address
-          baseModules
-          deployable
-          extraModules
-          hardwareModules
-          hostPlatform
-          hostname
-          monolithConfig
-          pubkey
-          remoteBuild
-          type
-          username
-          ;
-      }
-    else if type == "home-manager"
-    then
-      assert ((hasSuffix "linux" hostPlatform) || (hasSuffix "darwin" hostPlatform) && hostname == null);
-      assert pubkey == null; {
-        inherit
-          deployable
-          hostPlatform
-          hostRoles
-          hostname
-          type
-          username
-          ;
-      }
-    else throw "unknown host type '${type}'";
     roles = rec {
       rolesList = [
         "clockworkpi-dev"
