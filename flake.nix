f95e3b7f748719519ffd5678715dd1b4d52f24be
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-30 20:26:59 +0000

    feat: Uprev to NixOS 25.11
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
     };
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-compat = {
       url = "github:edolstra/flake-compat?ref=v1.1.0";
       flake = false;
