c3f43deb85affd90e12029aef72c194559f5b9d4
Dom Rodriguez <shymega@shymega.org.uk>
2025-12-26 18:01:54 +0000

    fix: Fix `flake-compat` path
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-compat = {
-      url = "github:edolstra/flake-compat?ref=v1.1.0";
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
     agenix = {
